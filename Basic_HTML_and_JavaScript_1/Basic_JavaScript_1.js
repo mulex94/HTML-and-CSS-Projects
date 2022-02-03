6b98071e46e43ebe6d2fe5f9ff4a1b3b0e447cac
Alexandru <menharth.alexandru@yahoo.com>
2022-02-03 15:47:37 +0000

    Basic_HTML_and_JavaScript_1
    Finish Submission Assignment  2

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+alert("Hello World!")
+
+function My_first_Function() {
+		var String = "KISS ME, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish") .innerHTML=
+		result;
+}
